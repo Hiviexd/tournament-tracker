18c5998578fb72019305a2262a3b94f1d7102e48
Hiviexd <rayenattia@gmail.com>
2024-05-02 01:16:33 +0100

    fix setsession maxAge not being set properly

--- server/helpers/index.ts
+++ server/helpers/index.ts
@@ -1,11 +1,12 @@
-import { IOsuAuthResponse } from '../../interfaces/OsuApi';
+import { IOsuAuthResponse } from "../../interfaces/OsuApi";
+import moment from "moment";
 
 function setSession(session, response: IOsuAuthResponse) {
-    const maxAge = new Date();
-    maxAge.setDate(maxAge.getDate() + 7);
-    session.cookie.maxAge = maxAge.getTime();
+    // set the cookie's maxAge to 7 days
+    session.cookie.maxAge = moment.duration(7, "days").asMilliseconds();
+
     // *1000 because maxAge is miliseconds, oauth is seconds
-    session.expireDate = Date.now() + (response.expires_in * 1000);
+    session.expireDate = Date.now() + response.expires_in * 1000;
     session.accessToken = response.access_token;
     session.refreshToken = response.refresh_token;
 }
@@ -14,13 +15,13 @@ function setSession(session, response: IOsuAuthResponse) {
 export function escapeUsername(username: string) {
     username = username.trim();
 
-    return username.replace(/[()[\]]/g, '\\$&');
+    return username.replace(/[()[\]]/g, "\\$&");
 }
 
-const defaultErrorMessage = { error: 'Something went wrong!' };
+const defaultErrorMessage = { error: "Something went wrong!" };
 
 export default {
     setSession,
     escapeUsername,
     defaultErrorMessage,
-}
+};
