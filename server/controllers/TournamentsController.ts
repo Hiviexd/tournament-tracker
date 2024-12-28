55c7447a1c4bdd13097854fee98a48590fa1d498
Hiviexd <rayenattia@gmail.com>
2024-12-29 00:17:15 +0100

    add search by modes

--- server/controllers/TournamentsController.ts
+++ server/controllers/TournamentsController.ts
@@ -1,6 +1,6 @@
 import Tournament from "../models/tournamentModel";
 import UserService from "../services/UserService";
-import { TournamentQueryParams, TournamentType } from "../../interfaces/Tournament";
+import { GameMode, TournamentQueryParams, TournamentType } from "../../interfaces/Tournament";
 import { UserGroup } from "../../interfaces/User";
 import User from "../models/userModel";
 
@@ -30,17 +30,19 @@ class TournamentsController {
 
     /** GET tournament listing */
     public async index(req, res) {
-        const { name, host, type, status, isActive, page = 1 } = req.query;
+        const { name, modes, host, type, status, isActive, page = 1 } = req.query;
         const query: TournamentQueryParams = {};
 
         if (name) query.name = new RegExp(name, "i");
-        if (type) query.type = type;
-        if (status) query.status = status;
-        if (isActive !== undefined) query.isActive = isActive === "true";
+        if (modes) query.modes = modes.split(",") as GameMode[];
         if (host) {
             const hostUser = await User.findByUsernameOrOsuId(host);
             query.host = hostUser;
         }
+        if (type) query.type = type;
+        if (status) query.status = status;
+        if (isActive !== undefined) query.isActive = isActive === "true";
+
 
         const skip = (Number(page) - 1) * this.defaultLimit;
         const isCommittee = res.locals.user.isCommittee;
