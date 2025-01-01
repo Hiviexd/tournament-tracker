156d0965b65ae726c5b0123ae10b5f9a8dd50525
Hiviexd <rayenattia@gmail.com>
2025-01-01 05:00:11 +0100

    create LogService

---
+++ server/services/LogService.ts
@@ -0,0 +1,27 @@
+import { LogCategory } from "../../interfaces/Log";
+import Log from "../models/logModel";
+
+class LogService {
+    /**
+     * Create a log for a user action
+     * @param user Action user
+     * @param action Action
+     * @param category Log category
+     */
+    public async generate(user: string, action: string, category: LogCategory): Promise<void> {
+        const log = new Log({ user, action, category });
+        await log.save();
+    }
+
+    /**
+     * Create a system log
+     * @param action Action
+     * @param category Log category
+     */
+    public async generateSystem(action: string, category: LogCategory): Promise<void> {
+        const log = new Log({ action, category });
+        await log.save();
+    }
+}
+
+export default new LogService();
