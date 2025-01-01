ff774ac17cd0c43a542a18b2acdc64dda701a5c9
Hiviexd <rayenattia@gmail.com>
2025-01-01 06:13:19 +0100

    bolder username

--- src/components/common/UserDisplay.tsx
+++ src/components/common/UserDisplay.tsx
@@ -12,7 +12,7 @@ export default function UserDisplay({ user }: IPropTypes) {
             <Avatar src={user.avatarUrl} size={40} radius="md" />
             <Stack gap={2}>
                 <Text
-                    fw={600}
+                    fw={700}
                     component="a"
                     href={`https://osu.ppy.sh/users/${user.osuId}`}
                     target="_blank">
