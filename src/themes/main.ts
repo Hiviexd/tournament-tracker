604335ab15be32634dac62d7fd4dd14ce02e3849
Hiviexd <rayenattia@gmail.com>
2025-01-01 06:13:12 +0100

    switch to Nunito font for now

--- src/themes/main.ts
+++ src/themes/main.ts
@@ -81,7 +81,7 @@ const warning: MantineColorsTuple = [
 ];
 
 export const theme = createTheme({
-    fontFamily: "Torus, sans-serif",
+    fontFamily: "Nunito, sans-serif",
     shadows: {
         md: "1px 1px 3px rgba(0, 0, 0, .25)",
         xl: "5px 5px 3px rgba(0, 0, 0, .25)",
