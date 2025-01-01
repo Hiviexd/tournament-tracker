50a0cd4902835aa014e056622fc0b306f989924c
Hiviexd <rayenattia@gmail.com>
2025-01-01 04:13:36 +0100

    remove unused middleware helper

--- server/middlewares/index.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-
-export function unauthorize(req: Request, res: Response) {
-    if (req.accepts(["html", "json"]) === "json") {
-        res.json({ error: "Unauthorized, login first" });
-    } else {
-        res.redirect("/");
-    }
-}
