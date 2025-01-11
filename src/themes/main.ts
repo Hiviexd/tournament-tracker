26db35de3a2ea04c0948fdcf3e3a1c00107f7a16
Hivie <me@hivie.tn>
2025-01-11 04:22:23 +0100

    less strong colors

--- src/themes/main.ts
+++ src/themes/main.ts
@@ -59,7 +59,7 @@ const hue = parseInt(localStorage.getItem("hue") || DEFAULT_HUE, 10);
 const isGreyscale = localStorage.getItem("greyscale") === "true";
 
 const generateTheme = (hue: number, isGreyscale: boolean) => {
-    const primaryHexColor = isGreyscale ? "#000000" : helpers.hslToHex(hue, 0.8, 0.5);
+    const primaryHexColor = isGreyscale ? "#000000" : helpers.hslToHex(hue, 0.5, 0.5);
 
     const theme = generateColors(primaryHexColor) as unknown as string[];
 
