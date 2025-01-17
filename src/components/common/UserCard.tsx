e80740e21f5ee06cfc95571425dd4f7985ffc744
Hivie <me@hivie.tn>
2025-01-17 02:51:20 +0100

    make a dedicated UserCard component

---
+++ src/components/common/UserCard.tsx
@@ -0,0 +1,38 @@
+import { Card } from "@mantine/core";
+import { IUser } from "../../../interfaces/User";
+import UserDisplay from "./UserDisplay";
+
+interface IProps {
+    user: IUser;
+    onSelect: (user: IUser) => void;
+}
+
+export default function UserCard({ user, onSelect }: IProps) {
+    return (
+        <Card
+            key={user._id}
+            shadow="sm"
+            p="md"
+            className="user-card"
+            style={{ minWidth: 240 }}
+            onClick={() => onSelect(user)}>
+            <div
+                style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    backgroundImage: `url(${user.coverUrl})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    filter: "brightness(0.4)",
+                    zIndex: 0,
+                }}
+            />
+            <div className="user-card-content">
+                <UserDisplay user={user} />
+            </div>
+        </Card>
+    );
+}
