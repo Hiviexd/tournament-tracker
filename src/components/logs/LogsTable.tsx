1057160719829ebe3aa572de636356a4fa46a33f
Hivie <me@hivie.tn>
2025-02-16 14:24:54 +0100

    design considerations for logs table at small widths

--- src/components/logs/LogsTable.tsx
+++ src/components/logs/LogsTable.tsx
@@ -1,4 +1,4 @@
-import { Table, Card, Tooltip, Text } from "@mantine/core";
+import { Table, Card, Tooltip, Text, ScrollArea } from "@mantine/core";
 import { ILog } from "../../../interfaces/Log";
 import moment from "moment";
 import MarkdownText from "../common/MarkdownText";
@@ -11,40 +11,42 @@ interface IProps {
 export default function LogsTable({ logs }: IProps) {
     return (
         <Card shadow="sm" p="lg">
-            <Table>
-                <Table.Thead>
-                    <Table.Tr>
-                        <Table.Th>Date</Table.Th>
-                        <Table.Th w={0} p={0}></Table.Th>
-                        <Table.Th>User</Table.Th>
-                        <Table.Th>Category</Table.Th>
-                        <Table.Th>Action</Table.Th>
-                    </Table.Tr>
-                </Table.Thead>
-                <Table.Tbody>
-                    {logs.map((log) => (
-                        <Table.Tr key={log._id}>
-                            <Table.Td>
-                                <Tooltip label={moment(log.createdAt).format("LLL")}>
-                                    <span>{moment(log.createdAt).fromNow()}</span>
-                                </Tooltip>
-                            </Table.Td>
-                            <Table.Td p={0}>
-                                {log.isSystemLog && <FontAwesomeIcon icon="robot" />}
-                            </Table.Td>
-                            <Table.Td>{log.user?.username || "System"}</Table.Td>
-                            <Table.Td>
-                                <Text size="sm" tt="capitalize">
-                                    {log.category}
-                                </Text>
-                            </Table.Td>
-                            <Table.Td>
-                                <MarkdownText content={log.action} />
-                            </Table.Td>
+            <ScrollArea>
+                <Table miw={800}>
+                    <Table.Thead>
+                        <Table.Tr>
+                            <Table.Th w={160}>Date</Table.Th>
+                            <Table.Th w={30} p={0}></Table.Th>
+                            <Table.Th w={150}>User</Table.Th>
+                            <Table.Th w={120}>Category</Table.Th>
+                            <Table.Th>Action</Table.Th>
                         </Table.Tr>
-                    ))}
-                </Table.Tbody>
-            </Table>
+                    </Table.Thead>
+                    <Table.Tbody>
+                        {logs.map((log) => (
+                            <Table.Tr key={log._id}>
+                                <Table.Td>
+                                    <Tooltip label={moment(log.createdAt).format("LLL")}>
+                                        <span>{moment(log.createdAt).fromNow()}</span>
+                                    </Tooltip>
+                                </Table.Td>
+                                <Table.Td p={0}>{log.isSystemLog && <FontAwesomeIcon icon="robot" />}</Table.Td>
+                                <Table.Td>
+                                    <Text truncate>{log.user?.username || "System"}</Text>
+                                </Table.Td>
+                                <Table.Td>
+                                    <Text size="sm" tt="capitalize">
+                                        {log.category}
+                                    </Text>
+                                </Table.Td>
+                                <Table.Td>
+                                    <MarkdownText content={log.action} />
+                                </Table.Td>
+                            </Table.Tr>
+                        ))}
+                    </Table.Tbody>
+                </Table>
+            </ScrollArea>
         </Card>
     );
 }
