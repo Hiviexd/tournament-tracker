7a5ae372d82dc5c5bb3d8edcaee1658655fbd8d9
Hivie <me@hivie.tn>
2025-02-23 22:37:59 +0100

    fix perm

--- src/pages/TicketDetailsPage.tsx
+++ src/pages/TicketDetailsPage.tsx
@@ -83,7 +83,8 @@ export default function TicketDetailsPage() {
         );
     }
 
-    const showMessageForm = ticket.isActive || user?.isCommittee;
+    // only show to author when ticket is active
+    const showMessageForm = (ticket.isActive && ticket.author.id === user?.id) || user?.isCommittee;
 
     return (
         <Stack gap="md">
