34582787887643e6e8faf6c00ade32c9bdb27004
Hivie <me@hivie.tn>
2025-02-28 00:49:12 +0100

    add stale tickets cronjob

--- server/services/AutomationService.ts
+++ server/services/AutomationService.ts
@@ -1,5 +1,6 @@
 import { CronJob } from "cron";
 import Voting from "../models/votingModel";
+import Ticket from "../models/ticketModel";
 import moment from "moment";
 import config from "../../config.json";
 import DiscordService from "./DiscordService";
@@ -11,6 +12,7 @@ import helpers from "../helpers/index";
 class AutomationService {
     private checkOverdueVotingsJob: CronJob;
     private checkConcludableVotingsJob: CronJob;
+    private checkStaleTicketsJob: CronJob;
 
     constructor() {
         // Run at 17:00 UTC every day
@@ -18,6 +20,9 @@ class AutomationService {
 
         // Run every hour
         this.checkConcludableVotingsJob = new CronJob("0 * * * *", this.checkConcludableVotings.bind(this));
+
+        // Run at 18:00 UTC every day
+        this.checkStaleTicketsJob = new CronJob("0 18 * * *", this.checkStaleTickets.bind(this));
     }
 
     public start() {
@@ -25,6 +30,7 @@ class AutomationService {
 
         this.checkOverdueVotingsJob.start();
         this.checkConcludableVotingsJob.start();
+        this.checkStaleTicketsJob.start();
 
         console.log(styles("✓ Automation service started!", ["green", "bold", "underline"]));
 
@@ -33,6 +39,7 @@ class AutomationService {
             console.log(styles("Running automation checks immediately...", ["yellow", "bold"]));
             this.checkOverdueVotings();
             this.checkConcludableVotings();
+            this.checkStaleTickets();
         }
     }
 
@@ -122,6 +129,65 @@ class AutomationService {
                     fields: [...fields],
                 },
             ]);
+        }
+    }
+
+    private async checkStaleTickets() {
+        const activeTickets = await Ticket.find({ isActive: true })
+            .populate("messages")
+            .populate("author", "username osuId")
+            .populate("targetUser", "username osuId");
+
+        for (const ticket of activeTickets) {
+            const lastResponse = moment(ticket.lastResponseAt);
+            const now = moment();
+            const daysSinceLastResponse = now.diff(lastResponse, "days");
+
+            // Skip if less than 7 days old
+            if (daysSinceLastResponse < 7) continue;
+
+            const roles: string[] = [];
+            if (ticket.assignedGroup === "tc") roles.push("tournament");
+            if (ticket.assignedGroup === "cc") roles.push("contest");
+
+            const ticketType = ticket.type === "report" ? "Report" : "Ticket";
+            const ticketUrl = `${config.baseUrl}/tickets/${ticket._id}`;
+
+            if (daysSinceLastResponse >= 10) {
+                // 10+ days - send with ping
+                await DiscordService.sendRoleHighlightWebhook(roles, [
+                    {
+                        color: webhookColors.red,
+                        description: `[**${ticket.title}**](${ticketUrl}) has had no response in ${daysSinceLastResponse} days!`,
+                        fields: [
+                            { name: "Type", value: ticketType, inline: true },
+                            { name: "Author", value: `[**${ticket.author.username}**](${ticket.author.osuProfileUrl})`, inline: true },
+                            {
+                                name: "Last Response",
+                                value: helpers.discordTimestamp(ticket.lastResponseAt),
+                                inline: false,
+                            },
+                        ],
+                    },
+                ]);
+            } else {
+                // 7-9 days - send without ping
+                await DiscordService.sendWebhook([
+                    {
+                        color: webhookColors.orange,
+                        description: `[**${ticket.title}**](${ticketUrl}) has had no response in ${daysSinceLastResponse} days`,
+                        fields: [
+                            { name: "Type", value: ticketType, inline: true },
+                            { name: "Author", value: ticket.author.username, inline: true },
+                            {
+                                name: "Last Response",
+                                value: helpers.discordTimestamp(ticket.lastResponseAt),
+                                inline: false,
+                            },
+                        ],
+                    },
+                ]);
+            }
         }
     }
 }
