39483b79a692a2e03ffcf7f43bd1c92d3c93b7ce
Hivie <me@hivie.tn>
2025-03-01 03:12:53 +0100

    try turning down saturation a bit

--- src/components/common/modals/ThemeCustomizeModal.tsx
+++ src/components/common/modals/ThemeCustomizeModal.tsx
@@ -27,7 +27,7 @@ export default function ThemeCustomizeModal({ opened, onClose }: IProps) {
 
     const getPreviewColor = (hue: number, isGreyscale: boolean) => {
         if (isGreyscale) return "#656565";
-        return `hsl(${hue}, 75%, 50%)`;
+        return `hsl(${hue}, 50%, 50%)`;
     };
 
     const handleSubmit = () => {
@@ -63,7 +63,8 @@ export default function ThemeCustomizeModal({ opened, onClose }: IProps) {
                     <Button
                         onClick={handleSubmit}
                         disabled={
-                            initialHue === newHue && isGreyscale === (localStorage.getItem("greyscale") === "true")
+                            initialHue === newHue &&
+                            isGreyscale === (localStorage.getItem("greyscale") === "true")
                         }
                         leftSection={<FontAwesomeIcon icon="palette" />}>
                         Apply Theme
