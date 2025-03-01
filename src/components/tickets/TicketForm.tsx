7879bd6264c423b65126e2ec0ed360b41a5a9fec
Hivie <me@hivie.tn>
2025-03-01 17:28:17 +0100

    fix link size in markdown display

--- src/components/tickets/TicketForm.tsx
+++ src/components/tickets/TicketForm.tsx
@@ -83,7 +83,7 @@ export default function TicketForm() {
                 <MarkdownText
                     content={`Tickets and their conversations are visible to the public! This is to provide an archive for users to look through when researching a specific issue or topic.
 
-Try searching for your issue in the **[Tickets listing](/tickets)** before creating a new ticket!`}
+Try searching for your issue in the [**Tickets listing**](/tickets) before creating a new ticket!`}
                 />
             </Alert>
 
