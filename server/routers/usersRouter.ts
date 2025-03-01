93012e30e7797467ff71329e836487cf96222530
Hivie <me@hivie.tn>
2025-03-01 18:49:04 +0100

    allow regular users to create users

--- server/routers/usersRouter.ts
+++ server/routers/usersRouter.ts
@@ -7,7 +7,7 @@ const usersRouter = express.Router();
 usersRouter.get("/", permissions.isLoggedIn, UsersController.index);
 usersRouter.get("/me", permissions.isLoggedIn, UsersController.getSelf);
 usersRouter.get("/getCommittee", UsersController.getCommittee);
-usersRouter.post("/create", permissions.isLoggedIn, permissions.isCommittee, UsersController.create);
+usersRouter.post("/create", permissions.isLoggedIn, UsersController.create);
 usersRouter.get("/:userInput", UsersController.getUser);
 usersRouter.post("/:userId/toggleReviewerStatus", permissions.isLoggedIn, permissions.isCommittee, UsersController.toggleReviewerStatus);
 usersRouter.post("/:userId/groupMove", permissions.isLoggedIn, permissions.isAdmin, UsersController.updateUserGroups);
