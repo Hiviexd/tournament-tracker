f83594e8c4e24e48f746d7384b59aa1cfab7f13b
Hivie <me@hivie.tn>
2025-03-23 02:47:41 +0100

    apparently i need to wrap this in a useEffect

--- src/base/VersionChecker.tsx
+++ src/base/VersionChecker.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Button } from "@mantine/core";
 import { notifications } from "@mantine/notifications";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -10,26 +11,28 @@ export default function VersionChecker() {
     const { data: version } = useVersion();
     const currentHash = __COMMIT_HASH__;
 
-    // Only show notification if we have both hashes and they don't match
-    if (version?.hash && currentHash && version.hash !== currentHash && version.hash !== "unknown") {
-        notifications.show({
-            id: "version-check",
-            color: "primary.10",
-            title: "Website out of date!",
-            message: (
-                <Button
-                    variant="filled"
-                    color="primary"
-                    onClick={() => window.location.reload()}
-                    mt="sm"
-                    leftSection={<FontAwesomeIcon icon="arrows-rotate" />}>
-                    Refresh to get the latest version
-                </Button>
-            ),
-            autoClose: false,
-            withCloseButton: false,
-        });
-    }
+    useEffect(() => {
+        // Only show notification if we have both hashes and they don't match
+        if (version?.hash && currentHash && version.hash !== currentHash && version.hash !== "unknown") {
+            notifications.show({
+                id: "version-check",
+                color: "primary.10",
+                title: "Website out of date!",
+                position: "bottom-center",
+                message: (
+                    <Button
+                        variant="filled"
+                        color="primary"
+                        onClick={() => window.location.reload()}
+                        leftSection={<FontAwesomeIcon icon="arrows-rotate" />}>
+                        Refresh to get the latest version
+                    </Button>
+                ),
+                autoClose: false,
+                withCloseButton: false,
+            });
+        }
+    }, [version?.hash, currentHash]);
 
     return null;
 }
