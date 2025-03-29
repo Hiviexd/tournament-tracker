2ba89581a262410eaab82c1cc3ea2228d92b2faf
Hivie <me@hivie.tn>
2025-03-29 15:10:47 +0100

    add tooltip to gamemode icon

--- src/components/common/GameModeIcon.tsx
+++ src/components/common/GameModeIcon.tsx
@@ -1,20 +1,40 @@
 import { GameMode } from "../../../interfaces/Tournament";
 import { Group } from "@mantine/core";
+import { Tooltip } from "@mantine/core";
 
 interface Props {
     mode: GameMode | GameMode[];
 }
 
 export default function GameModeIcon({ mode }: Props) {
+    function getModeName(mode: GameMode) {
+        switch (mode) {
+            case "osu":
+                return "osu!";
+            case "taiko":
+                return "osu!taiko";
+            case "catch":
+                return "osu!catch";
+            case "mania":
+                return "osu!mania";
+        }
+    }
+
     if (Array.isArray(mode)) {
         return (
             <Group gap={0} wrap="nowrap">
                 {mode.map((m) => (
-                    <div key={m} className={`gamemode-icon ${m}`} />
+                    <Tooltip label={getModeName(m)}>
+                        <div key={m} className={`gamemode-icon ${m}`} />
+                    </Tooltip>
                 ))}
             </Group>
         );
     }
 
-    return <div className={`gamemode-icon ${mode}`} />;
+    return (
+        <Tooltip label={getModeName(mode)}>
+            <div className={`gamemode-icon ${mode}`} />
+        </Tooltip>
+    );
 }
