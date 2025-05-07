6171c9c49df0beb701bccb3ed5d37dd08600be0b
Hivie <me@hivie.tn>
2025-05-07 19:03:23 +0100

    update debug api handling

--- src/hooks/useDebug.ts
+++ src/hooks/useDebug.ts
@@ -1,20 +1,27 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { getSession, updateSession, UpdateSessionBody } from "../api/debug";
 import utils from "../../utils";
 
 export function useSession() {
     return useQuery({
         queryKey: ["session"],
-        queryFn: getSession,
+        queryFn: () =>
+            utils.apiCall({
+                method: "get",
+                url: "/api/dev/session",
+            }),
     });
 }
 
-export function useUpdateSession(data: UpdateSessionBody) {
+export function useUpdateSession(data: { mongoId: string; osuId: string; username: string }) {
     const queryClient = useQueryClient();
 
     return useMutation({
         mutationFn: async () => {
-            const response = await updateSession(data);
+            const response = await utils.apiCall({
+                method: "post",
+                url: "/api/dev/session/update",
+                data,
+            });
             return utils.handleMutationResponse(response);
         },
         onSuccess: () => {
