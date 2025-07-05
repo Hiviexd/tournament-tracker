d3127cb989e4df0208c17570d10fb88ea69bccd2
Hivie <me@hivie.tn>
2025-07-05 04:09:31 +0100

    refactor footer

--- src/components/base/Footer.tsx
+++ src/components/base/Footer.tsx
@@ -1,25 +1,46 @@
-import { Group, Text } from "@mantine/core";
+import { Group, Button } from "@mantine/core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+
+interface FooterItem {
+    icon: IconProp;
+    label: string;
+    href: string;
+}
 
 export default function Footer() {
+    const footerItems: FooterItem[] = [
+        {
+            icon: ["fab", "github"],
+            label: "Source Code",
+            href: "https://github.com/Hiviexd/tournament-tracker",
+        },
+        {
+            icon: "bug",
+            label: "Report Issues",
+            href: "https://github.com/Hiviexd/tournament-tracker/issues/new",
+        },
+        {
+            icon: "users",
+            label: "The Team",
+            href: "https://osu.ppy.sh/wiki/en/People/Tournament_Committee",
+        },
+    ];
+
     return (
-        <Group gap="xl" align="center" justify="center" mt="lg">
-            <Text
-                c="primary"
-                size="sm"
-                component="a"
-                href="https://github.com/Hiviexd/tournament-tracker"
-                target="_blank">
-                <FontAwesomeIcon icon={["fab", "github"]} /> Source Code
-            </Text>
-            <Text
-                c="primary"
-                size="sm"
-                component="a"
-                href="https://github.com/Hiviexd/tournament-tracker/issues/new"
-                target="_blank">
-                <FontAwesomeIcon icon="bug" /> Report Issues
-            </Text>
+        <Group gap="xs" align="center" justify="center" mt="lg">
+            {footerItems.map((item) => (
+                <Button
+                    key={item.label}
+                    variant="subtle"
+                    size="sm"
+                    component="a"
+                    href={item.href}
+                    target="_blank"
+                    leftSection={<FontAwesomeIcon icon={item.icon} />}>
+                    {item.label}
+                </Button>
+            ))}
         </Group>
     );
 }
