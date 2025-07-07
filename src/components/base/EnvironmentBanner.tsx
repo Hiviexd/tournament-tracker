0dfd986e229006a9f5a87cd6bf42026c369b22b4
Hivie <me@hivie.tn>
2025-07-08 00:53:37 +0100

    i love testing in prod

--- src/components/base/EnvironmentBanner.tsx
+++ src/components/base/EnvironmentBanner.tsx
@@ -11,6 +11,8 @@ export default function EnvironmentBanner() {
     const isPreview = import.meta.env.MODE === "preview";
     const isProduction = import.meta.env.PROD && !isDevelopment && !isPreview;
 
+    console.log(import.meta.env);
+
     if (isProduction) {
         return null;
     }
