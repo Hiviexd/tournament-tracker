84fa6d42052f15dddfec02b8d55254c850ef9b59
Hivie <me@hivie.tn>
2025-08-31 21:59:44 +0100

    shorten column width for less breakage

--- src/components/tournaments/TournamentTable.tsx
+++ src/components/tournaments/TournamentTable.tsx
@@ -59,7 +59,7 @@ export default function TournamentTable({ tournaments, total, currentPage }: IPr
                                         <GameModeIcon mode={tournament.modes} />
                                     </Table.Td>
 
-                                    <Table.Td w={400}>
+                                    <Table.Td w={350}>
                                         <Link to={`/tournaments/${tournament._id}`}>
                                             <TruncatedText lineClamp={1} textProps={{ fw: 500 }}>
                                                 {tournament.name}
@@ -75,7 +75,10 @@ export default function TournamentTable({ tournaments, total, currentPage }: IPr
                                         <Group gap="xs">
                                             <TournamentStatusBadge status={tournament.status} />
                                             {user?.isCommittee && potentiallyNeedsReview(tournament) && (
-                                                <Tooltip multiline w={220} label="Tournament ended, potentially movable to review phase">
+                                                <Tooltip
+                                                    multiline
+                                                    w={220}
+                                                    label="Tournament ended, potentially movable to review phase">
                                                     <Badge color="yellow" variant="light">
                                                         <FontAwesomeIcon icon="exclamation-triangle" />
                                                     </Badge>
