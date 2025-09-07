58163bdb46d5757bdd1d4ecae067ab954fac5b9a
Hivie <me@hivie.tn>
2025-09-07 04:22:45 +0100

    enforce scopes enum in model

--- server/models/apiKeyModel.ts
+++ server/models/apiKeyModel.ts
@@ -1,12 +1,12 @@
 import mongoose, { Schema } from "mongoose";
-import { IApiKey } from "../../interfaces/ApiKey";
+import { IApiKey, AvailableApiScopes } from "../../interfaces/ApiKey";
 
 const ApiKeySchema = new Schema<IApiKey>(
     {
         user: { type: Schema.Types.ObjectId, ref: "User", required: true },
         hashedKey: { type: String, required: true, unique: true, index: true },
         name: { type: String, required: true },
-        scopes: { type: [String], default: [] },
+        scopes: { type: [String], default: [], enum: Object.values(AvailableApiScopes) },
         createdAt: { type: Date, default: () => new Date() },
         lastUsed: { type: Date },
         revokedAt: { type: Date, default: null },
