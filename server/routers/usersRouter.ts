4f7b4b4fe23a60553ca61b783c52b96465768d1b
Hivie <me@hivie.tn>
2025-09-07 04:23:05 +0100

    use correct auth middleware

--- server/routers/usersRouter.ts
+++ server/routers/usersRouter.ts
@@ -7,7 +7,7 @@ import { requireScopes } from "../middlewares/authenticateRequest";
 const usersRouter = express.Router();
 
 usersRouter.get("/", auth.optionalAuth, UsersController.index);
-usersRouter.get("/me", requireScopes(["users:read"]), auth.optionalAuth, auth.isLoggedIn, UsersController.getSelf);
+usersRouter.get("/me", requireScopes(["users:read"]), auth.isLoggedIn, UsersController.getSelf);
 usersRouter.get("/getCommittee", auth.optionalAuth, UsersController.getCommittee);
 usersRouter.post("/create", auth.isLoggedIn, UsersController.create);
 usersRouter.get("/:userInput", auth.optionalAuth, UsersController.getUser);
