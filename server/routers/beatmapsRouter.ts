3b2285fe13c8eebe004ce42c6897e1cfb35204b6
Hivie <me@hivie.tn>
2025-09-08 03:20:30 +0100

    gate compliance tool access with auth
    part of the future plan of allowing this to be used by sheets as an api

--- server/routers/beatmapsRouter.ts
+++ server/routers/beatmapsRouter.ts
@@ -1,9 +1,11 @@
 // @ts-nocheck
 import { Router } from "express";
+import auth from "../middlewares/auth";
+import { requireScopes } from "../middlewares/authenticateRequest";
 import BeatmapsController from "../controllers/BeatmapsController";
 
 const beatmapsRouter = Router();
 
-beatmapsRouter.post("/check", BeatmapsController.checkMappoolCompliance);
+beatmapsRouter.post("/check",requireScopes(["beatmaps:read"]), auth.isLoggedIn, BeatmapsController.checkMappoolCompliance);
 
 export default beatmapsRouter;
