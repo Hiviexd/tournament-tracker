df70ddb4f69a6d41dc060eb735bd92eec07888d1
Hivie <me@hivie.tn>
2025-09-14 00:33:04 +0100

    fix api scope enum mismatch

--- interfaces/ApiKey.ts
+++ interfaces/ApiKey.ts
@@ -6,6 +6,16 @@ type ApiScopeAction = "read" | "write";
 
 export type ApiScope = `${ApiScopeCategory}:${ApiScopeAction}`;
 
+export enum AllApiScopes {
+    BEATMAPS_READ = "beatmaps:read", // ! Removed 2025-09-14
+    COMPLIANCE_READ = "compliance:read", // ! Added 2025-09-14
+    TOURNAMENTS_READ = "tournaments:read",
+    VOTINGS_READ = "votings:read",
+    RESOURCES_READ = "resources:read",
+    USERS_READ = "users:read",
+    TICKETS_READ = "tickets:read",
+}
+
 export enum AvailableApiScopes {
     COMPLIANCE_READ = "compliance:read",
     TOURNAMENTS_READ = "tournaments:read",
--- server/models/apiKeyModel.ts
+++ server/models/apiKeyModel.ts
@@ -1,12 +1,12 @@
 import mongoose, { Schema } from "mongoose";
-import { IApiKey, AvailableApiScopes } from "../../interfaces/ApiKey";
+import { IApiKey, AllApiScopes } from "../../interfaces/ApiKey";
 
 const ApiKeySchema = new Schema<IApiKey>(
     {
         user: { type: Schema.Types.ObjectId, ref: "User", required: true },
         hashedKey: { type: String, required: true, unique: true, index: true },
         name: { type: String, required: true },
-        scopes: { type: [String], default: [], enum: Object.values(AvailableApiScopes) },
+        scopes: { type: [String], default: [], enum: Object.values(AllApiScopes) },
         createdAt: { type: Date, default: () => new Date() },
         lastUsedAt: { type: Date },
         timesUsed: { type: Number, default: 0 },
