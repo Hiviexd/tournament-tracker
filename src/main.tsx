1b3f1ea832775c0451b436681f051a1e98852887
Hivie <me@hivie.tn>
2025-09-15 02:01:26 +0100

    add mantine spotlight component

--- src/main.tsx
+++ src/main.tsx
@@ -20,6 +20,8 @@ import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
 import "@mantine/dates/styles.css";
 import "@mantine/charts/styles.css";
+import "@mantine/spotlight/styles.css";
+
 import "./sass/app.scss";
 
 // Layout
