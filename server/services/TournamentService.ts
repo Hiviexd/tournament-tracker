d73a8aea5a59a599d08b7ea165a35415037736f5
Hivie <me@hivie.tn>
2025-09-27 23:08:42 +0100

    missing flag

--- server/services/TournamentService.ts
+++ server/services/TournamentService.ts
@@ -106,7 +106,7 @@ class TournamentService {
         }).populate([
             {
                 path: "author",
-                select: "username osuId groups coverUrl",
+                select: "username osuId groups coverUrl country",
             },
         ]);
 
@@ -152,7 +152,7 @@ class TournamentService {
         }).populate([
             {
                 path: "author",
-                select: "username osuId groups coverUrl",
+                select: "username osuId groups coverUrl country",
             },
         ]);
 
