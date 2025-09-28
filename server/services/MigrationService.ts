a6c1ad5216912ac77d77d382a9f66b4a596a8664
Hivie <me@hivie.tn>
2025-09-28 16:00:50 +0100

    remove directive for now

--- server/services/MigrationService.ts
+++ server/services/MigrationService.ts
@@ -346,7 +346,6 @@ class MigrationService {
             for (const tournament of tournamentsToMigrate) {
                 try {
                     // Convert single host to hosts array
-                    // @ts-expect-error - tournament.host is a thing pre-migration
                     const hostId = tournament.host;
                     if (hostId) {
                         // Set the hosts array with the single host
