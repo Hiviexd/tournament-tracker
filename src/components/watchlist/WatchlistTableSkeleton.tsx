0643e45cf239f8e4650a702c0a61a7c8b38db3cd
Hivie <me@hivie.tn>
2026-04-23 22:54:17 +0100

    update watchlist skeleton

--- src/components/watchlist/WatchlistTableSkeleton.tsx
+++ src/components/watchlist/WatchlistTableSkeleton.tsx
@@ -1,4 +1,4 @@
-import { Card, ScrollArea, Table, Skeleton } from "@mantine/core";
+import { Card, ScrollArea, Table, Skeleton, Group, Stack } from "@mantine/core";
 
 export default function WatchlistTableSkeleton() {
     return (
@@ -17,11 +17,35 @@ export default function WatchlistTableSkeleton() {
                     <Table.Tbody>
                         {Array.from({ length: 10 }).map((_, i) => (
                             <Table.Tr key={i}>
-                                <Table.Td><Skeleton height={20} width={120} /></Table.Td>
-                                <Table.Td><Skeleton height={20} width={100} /></Table.Td>
-                                <Table.Td><Skeleton height={20} width={90} /></Table.Td>
-                                <Table.Td ta="center"><Skeleton height={20} width={40} /></Table.Td>
-                                <Table.Td ta="center"><Skeleton height={20} width={40} /></Table.Td>
+                                <Table.Td>
+                                    <Group gap="sm" wrap="nowrap" align="center">
+                                        <Skeleton circle height={40} />
+                                        <Stack gap={4} style={{ minWidth: 0 }}>
+                                            <Skeleton height={16} width={Math.min(160, 120 + (i % 4) * 12)} />
+                                        </Stack>
+                                    </Group>
+                                </Table.Td>
+                                <Table.Td>
+                                    <Group gap="xs" wrap="nowrap">
+                                        <Skeleton height={28} width={88 + (i % 3) * 8} radius="xl" />
+                                        <Skeleton height={22} width={64} radius="xl" />
+                                    </Group>
+                                </Table.Td>
+                                <Table.Td>
+                                    <Group gap={4} wrap="nowrap">
+                                        <Skeleton height={28} width={92 + (i % 2) * 10} radius="xl" />
+                                    </Group>
+                                </Table.Td>
+                                <Table.Td ta="center">
+                                    <Group justify="center" wrap="nowrap">
+                                        <Skeleton circle height={24} />
+                                    </Group>
+                                </Table.Td>
+                                <Table.Td ta="center">
+                                    <Group justify="center" wrap="nowrap">
+                                        <Skeleton circle height={24} />
+                                    </Group>
+                                </Table.Td>
                             </Table.Tr>
                         ))}
                     </Table.Tbody>
